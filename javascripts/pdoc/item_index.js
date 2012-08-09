34e94b49acf99b8a17177282025080490b338f1c
Thomas Debarochez <thomas.barochez+github@gmail.com>
2012-08-10 01:32:52 +0200

    add doc

---
+++ javascripts/pdoc/item_index.js
@@ -0,0 +1,20 @@
+if (!window.PDoc) window.PDoc = {};
+PDoc.elements = {
+  'Client#setStatus': { 'name': 'Client#setStatus', 'type': 'instance method', 'path': 'xmpp/Client/prototype/setStatus/index.html' },
+'Client#roster': { 'name': 'Client#roster', 'type': 'instance property', 'path': 'xmpp/Client/prototype/roster/index.html' },
+'new Client': { 'name': 'new Client', 'type': 'constructor', 'path': 'xmpp/Client/new/index.html' },
+'Client#push': { 'name': 'Client#push', 'type': 'instance method', 'path': 'xmpp/Client/prototype/push/index.html' },
+'Client#write': { 'name': 'Client#write', 'type': 'instance method', 'path': 'xmpp/Client/prototype/write/index.html' },
+'Client#setVcard': { 'name': 'Client#setVcard', 'type': 'instance method', 'path': 'xmpp/Client/prototype/setVcard/index.html' },
+'XMPP section': { 'name': 'XMPP section', 'type': 'section', 'path': 'xmpp/index.html' },
+'Client#subscribe': { 'name': 'Client#subscribe', 'type': 'instance method', 'path': 'xmpp/Client/prototype/subscribe/index.html' },
+'Client#pushHtml': { 'name': 'Client#pushHtml', 'type': 'instance method', 'path': 'xmpp/Client/prototype/pushHtml/index.html' },
+'Client#getRoster': { 'name': 'Client#getRoster', 'type': 'instance method', 'path': 'xmpp/Client/prototype/getRoster/index.html' },
+'Client#last_time_received': { 'name': 'Client#last_time_received', 'type': 'instance property', 'path': 'xmpp/Client/prototype/last_time_received/index.html' },
+'Client#id': { 'name': 'Client#id', 'type': 'instance property', 'path': 'xmpp/Client/prototype/id/index.html' },
+'Client#jid': { 'name': 'Client#jid', 'type': 'instance property', 'path': 'xmpp/Client/prototype/jid/index.html' },
+'Client#getBareJid': { 'name': 'Client#getBareJid', 'type': 'instance method', 'path': 'xmpp/Client/prototype/getBareJid/index.html' },
+'Client': { 'name': 'Client', 'type': 'class', 'path': 'xmpp/Client/index.html' },
+'Client#sendIq': { 'name': 'Client#sendIq', 'type': 'instance method', 'path': 'xmpp/Client/prototype/sendIq/index.html' },
+'Client#getVCard': { 'name': 'Client#getVCard', 'type': 'instance method', 'path': 'xmpp/Client/prototype/getVCard/index.html' }
+};
