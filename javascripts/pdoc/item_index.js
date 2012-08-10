53d6cc7da81d49a70e2b6342a663981c6f9810d8
Thomas Debarochez <thomas.barochez+github@gmail.com>
2012-08-10 09:07:03 +0200

    add analytics to every pages & gihub ribbon

--- javascripts/pdoc/item_index.js
+++ javascripts/pdoc/item_index.js
@@ -1,20 +1,20 @@
 if (!window.PDoc) window.PDoc = {};
 PDoc.elements = {
-  'Client#sendIq': { 'name': 'Client#sendIq', 'type': 'instance method', 'path': 'jacasr/Client/prototype/sendIq/index.html' },
+  'Client#write': { 'name': 'Client#write', 'type': 'instance method', 'path': 'jacasr/Client/prototype/write/index.html' },
+'Client#id': { 'name': 'Client#id', 'type': 'instance property', 'path': 'jacasr/Client/prototype/id/index.html' },
+'Client#push': { 'name': 'Client#push', 'type': 'instance method', 'path': 'jacasr/Client/prototype/push/index.html' },
 'Client#getBareJid': { 'name': 'Client#getBareJid', 'type': 'instance method', 'path': 'jacasr/Client/prototype/getBareJid/index.html' },
-'Client#setStatus': { 'name': 'Client#setStatus', 'type': 'instance method', 'path': 'jacasr/Client/prototype/setStatus/index.html' },
-'Client#subscribe': { 'name': 'Client#subscribe', 'type': 'instance method', 'path': 'jacasr/Client/prototype/subscribe/index.html' },
-'Client#getRoster': { 'name': 'Client#getRoster', 'type': 'instance method', 'path': 'jacasr/Client/prototype/getRoster/index.html' },
 'Client#roster': { 'name': 'Client#roster', 'type': 'instance property', 'path': 'jacasr/Client/prototype/roster/index.html' },
 'Jacasr section': { 'name': 'Jacasr section', 'type': 'section', 'path': 'jacasr/index.html' },
+'Client#getRoster': { 'name': 'Client#getRoster', 'type': 'instance method', 'path': 'jacasr/Client/prototype/getRoster/index.html' },
 'Client#jid': { 'name': 'Client#jid', 'type': 'instance property', 'path': 'jacasr/Client/prototype/jid/index.html' },
-'Client#setVcard': { 'name': 'Client#setVcard', 'type': 'instance method', 'path': 'jacasr/Client/prototype/setVcard/index.html' },
-'Client': { 'name': 'Client', 'type': 'class', 'path': 'jacasr/Client/index.html' },
-'Client#write': { 'name': 'Client#write', 'type': 'instance method', 'path': 'jacasr/Client/prototype/write/index.html' },
 'Client#last_time_received': { 'name': 'Client#last_time_received', 'type': 'instance property', 'path': 'jacasr/Client/prototype/last_time_received/index.html' },
-'Client#push': { 'name': 'Client#push', 'type': 'instance method', 'path': 'jacasr/Client/prototype/push/index.html' },
-'Client#id': { 'name': 'Client#id', 'type': 'instance property', 'path': 'jacasr/Client/prototype/id/index.html' },
-'new Client': { 'name': 'new Client', 'type': 'constructor', 'path': 'jacasr/Client/new/index.html' },
+'Client#sendIq': { 'name': 'Client#sendIq', 'type': 'instance method', 'path': 'jacasr/Client/prototype/sendIq/index.html' },
 'Client#pushHtml': { 'name': 'Client#pushHtml', 'type': 'instance method', 'path': 'jacasr/Client/prototype/pushHtml/index.html' },
-'Client#getVCard': { 'name': 'Client#getVCard', 'type': 'instance method', 'path': 'jacasr/Client/prototype/getVCard/index.html' }
+'Client#subscribe': { 'name': 'Client#subscribe', 'type': 'instance method', 'path': 'jacasr/Client/prototype/subscribe/index.html' },
+'Client#getVCard': { 'name': 'Client#getVCard', 'type': 'instance method', 'path': 'jacasr/Client/prototype/getVCard/index.html' },
+'new Client': { 'name': 'new Client', 'type': 'constructor', 'path': 'jacasr/Client/new/index.html' },
+'Client#setStatus': { 'name': 'Client#setStatus', 'type': 'instance method', 'path': 'jacasr/Client/prototype/setStatus/index.html' },
+'Client#setVcard': { 'name': 'Client#setVcard', 'type': 'instance method', 'path': 'jacasr/Client/prototype/setVcard/index.html' },
+'Client': { 'name': 'Client', 'type': 'class', 'path': 'jacasr/Client/index.html' }
 };
